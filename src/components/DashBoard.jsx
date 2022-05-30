1ba4aceed40854e65343a6bfa7092ff4989b1c11
IndhumathiRadhakrishnan <indhukrishnan1998.ik@gmail.com>
2022-05-30 19:29:14 +0530

    SecurityIssues

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,6 +1,21 @@
 import React, { useEffect } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import _ from 'lodash';
+import aperture from '../Assets/Icons/aperture-outline.svg'
+import search from '../Assets/Icons/search-outline.svg'
+import gift from '../Assets/Icons/gift-outline.svg'
+import bag from '../Assets/Icons/bag-add-outline.svg'
+import chevron from '../Assets/Icons/chevron-down-outline.svg'
+import menu from '../Assets/Icons/menu-outline.svg'
+import filter from '../Assets/Icons/filter-outline.svg'
+import cloud from '../Assets/Icons/cloud-upload-outline.svg'
+import star from '../Assets/Icons/star-outline.svg'
+import ellipsis from '../Assets/Icons/ellipsis-vertical-outline.svg'
+
+
+
+
+
 
 function DashBoard() {
     const [searchName, setSearchName] = React.useState('');
@@ -66,42 +81,36 @@ function DashBoard() {
             return prev;
         })
     }
-    
-
     return (
         <div className='flex flex-col h-screen '>
             <div className="w-full h-16 bg-white px-5 flex flex-row items-center justify-between">
                 <div className="flex gap-x-2 items-center ">
-                    <ion-icon name="aperture-outline" class="w-8 h-8"></ion-icon>
+                    <img src={aperture} className="w-8 h-8" alt="aperture" />
                     <div className=" ">iamneo.ai </div>
                     <div className="text-lg font-semibold ">Talent Center</div>
                 </div>
                 <div className="">
                     <div className="flex text-lg font-semibold gap-x-2">
-                        <div className="gap-x-2 flex items-center">
-                            <ion-icon name="search-outline"></ion-icon>
+                        <div className="gap-x-2 flex items-center">               
+                            <img src={search} className="w-8 h-8" alt="aperture" />
                             <input type="search" className="focus:outline-none border-b border-gray-200" name="" id="" placeholder='Search by Name' onChange={(e)=>{
                                 setSearchName(e.target.value)
                             }} />
                         </div>
-
                         <button className='p-1 px-2 bg-indigo-900 text-white capitalize rounded-md text-sm flex items-center'><ion-icon name="add-outline" ></ion-icon> Add New</button>
                         <span class="relative inline-block">
-                            <ion-icon name="gift-outline"
-                                class="w-6 h-6  hover:bg-gray-100/20 rounded-full fill-current p-2">
-                            </ion-icon>
+                            <img src={gift} className="w-6 h-6  hover:bg-gray-100/20 rounded-full fill-current p-2" alt="aperture" />
                             <span class="absolute top-2 right-1 inline-flex  px-1.5 py-1 text-xs font-bold leading-none transform translate-x-1/2 -translate-y-1/2 text-white bg-red-600 rounded-full">2</span>
                         </span>
                         <div className="h-10 w-10 rounded-full bg-green-100 flex items-center justify-center">S</div>
-
                     </div>
                 </div>
             </div>
             <div className="w-full h-16 bg-gray-300/80 px-5 flex flex-row items-center justify-between">
                 <div className="flex gap-x-2 items-center ">
-                    <ion-icon name="bag-handle-outline"></ion-icon>
+                    <img src={bag} className="w-8 h-8" alt="aperture" />
                     <div className=" ">Jobs</div>
-                    <ion-icon name="chevron-forward-outline"></ion-icon>
+                    <img src={chevron} className="w-8 h-8" alt="aperture" />
                     <div className='font-semibold text-lg'>Full-Stack Engineer</div>
                     <div className="border border-gray-200 rounded px-2 bg-white ">View Job Details</div>
                 </div>
@@ -111,23 +120,20 @@ function DashBoard() {
                         <option value="">Add Candidates</option>
                         <option value="">Add Candidates</option>
                     </select>
-
                     <button className='p-1 px-2 bg-indigo-900 text-white capitalize rounded-md text-sm flex items-center'>
-                        Published<ion-icon name="chevron-down-outline"></ion-icon>
+                        Published<img src={chevron} className="w-8 h-8" alt="chevron" /> 
                     </button>
                 </div>
             </div>
             <div className="w-full h-16 px-5 flex flex-row items-center justify-between">
-                <div className="">All Candidates -  Active (48) <ion-icon name="chevron-down-outline"></ion-icon></div>
-                <div className="">Sort By - Last Updated <ion-icon name="chevron-down-outline"></ion-icon></div>
+                <div className="flex gap-0.5">All Candidates -  Active (48) <img src={chevron} className=" w-6 h-6" alt="chevron" /></div>
+                <div className="flex gap-0.5">Sort By - Last Updated <img src={chevron} className=" w-6 h-6" alt="chevron" /></div>
                 <div className="flex gap-x-2">
-                    <ion-icon name="menu-outline"></ion-icon>
-                    <ion-icon name="filter-outline"></ion-icon>
-                    <ion-icon name="cloud-upload-outline"></ion-icon>
+                    <img src={menu} className="w-8 h-8" alt="menu" />
+                    <img src={filter} className="w-8 h-8" alt="menu" />
+                    <img src={cloud} className="w-8 h-8" alt="menu" />
                 </div>
             </div>
-
-
             <div className="h-full px-4 ml-2 flex overflow-auto ">
                 <DragDropContext onDragEnd={handleDragEnd}>
                     {
@@ -157,42 +163,35 @@ function DashBoard() {
                                                                                     <div className="font-semibold text-blue-900">{element.name.first}</div>
                                                                                     <div className="truncate">{element.email}</div>
                                                                                 </div>
-
                                                                                 <div className="bg-gray-100 h-10 flex items-center px-1 justify-between">
-                                                                                    <div className="">
-                                                                                        <ion-icon name="star-outline"></ion-icon>
-                                                                                        <ion-icon name="star-outline"></ion-icon>
-                                                                                        <ion-icon name="star-outline"></ion-icon>
-                                                                                        <ion-icon name="star-outline"></ion-icon>
+                                                                                    <div className="flex gap-0.5">
+                                                                                        <img src={star} className="w-6 h-6" alt="star" />
+                                                                                        <img src={star} className="w-6 h-6" alt="star" />
+                                                                                        <img src={star} className="w-6 h-6" alt="star" />
+                                                                                        <img src={star} className="w-6 h-6" alt="star" />
                                                                                     </div>
-
                                                                                     <div className="flex flex-row items-center gap-x-1">
                                                                                         <img className="rounded-full h-6 w-6" src={element.picture.medium} alt='' />
-                                                                                        <ion-icon name="ellipsis-vertical-outline"></ion-icon>
+                                                                                        <img src={ellipsis} className="w-6 h-6" alt="star" />
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
                                                                         )
                                                                     }}
-
                                                                 </Draggable>
                                                             )
                                                         })
-
                                                     }
                                                     {provided.placeholder}
                                                 </div>
                                             )
                                         }}
-
-
                                     </Droppable>
                                 </div>
                             )
                         })
                     }
-                </DragDropContext>
-                
+                </DragDropContext> 
             </div>
         </div>
     )
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,16 +1,22 @@
 import { NavLink, Outlet } from "react-router-dom";
+import aperture from '../Assets/Icons/aperture-outline.svg'
+import speedometer from '../Assets/Icons/speedometer-outline.svg'
+import file from '../Assets/Icons/file-tray-full-outline.svg'
+import bag from '../Assets/Icons/bag-add-outline.svg'
+import people from '../Assets/Icons/people-outline.svg'
+import settings from '../Assets/Icons/settings-outline.svg'
+
 const Sidebar = () => {
   const Menus = [
-    { title: "Performance", icon: "speedometer-outline", url: "#" },
-    { title: "Requests", icon: "file-tray-full-outline", url: "#" },
-    { title: "Projects", icon: "bag-add-outline", url: "#" },
-    { title: "Candidates", icon: "people-outline", url: "#" },
-    { title: "Settings", icon: "settings-outline", url: "#" },
+    { title: "Performance", icon: speedometer, url: "#" },
+    { title: "Requests", icon: file, url: "#" },
+    { title: "Projects", icon: bag, url: "#" },
+    { title: "Candidates", icon: people, url: "#" },
+    { title: "Settings", icon: settings, url: "#" },
   ];
   const style = {
     navLink: `flex rounded-lg rounded-r-none p-2 py-3 cursor-pointer hover:text-black text-BeeMG-dark-gray text-sm items-center gap-x-4 hover:bg-gradient-to-r from-BeeMG-yellow/50 border-BeeMG-yellow font-semibold justify-center `,
   }
-
   return (
     <> 
       <div className='flex flex-col h-screen w-full'>
@@ -18,8 +24,7 @@ const Sidebar = () => {
           <div className={` "lg:w-20 md:w-16 w-16"} bg-sky-900 h-screen pt-6 relative duration-300 shadow-md `}>
             <div className="flex gap-x-4 items-center justify-center">
               <div>
-                <ion-icon name="aperture-outline" class="h-8 w-8 hover:bg-gray-100/20 rounded-full p-2 text-white">
-                </ion-icon>
+                <img src={aperture} alt="aperture" className="h-8 w-8 hover:bg-gray-100/20 rounded-full text-white"/>
               </div>
             </div>
             <ul className="pl-2 pt-6 flex flex-col gap-1  ">
@@ -28,7 +33,7 @@ const Sidebar = () => {
                   key={index}
                   className={ style.navLink}
                 >
-                  <ion-icon name={Menu.icon} class="h-6 w-6 text-white" ></ion-icon>
+                  <img src={Menu.icon} className="h-6 w-6 text-white" alt={Menu.title} />
                   <div className="flex flex-row flex-none gap-1">
                     <span className={`hidden origin-left duration-200 capitalize truncate text-white`}>
                       {Menu.title}
